e9cf30732bf0c77304c89e84024f8234fe2cb48c
Fe198 <40144601+Fe198@users.noreply.github.com>
2022-04-26 11:27:40 -0300

    update main.py

--- ex045/main.py
+++ ex045/main.py
@@ -8,8 +8,19 @@ print("""Suas opções:
 player = input("Qual é a sua jogada? ")
 computer = random.randint(0, 2)
 
-if player == 2 and computer == 1 or player == 1 and computer == 0  or player == 0 and computer == 2:
+# situacoes que o player vence
+if player == 2 and computer == 1:
     print("Jogador vence")
-elif player == 1 and computer == 2 or player == 0 and computer == 1 or player == 2 and computer == 0:
+elif player == 1 and computer == 0:
+    print("Jogador vence")
+elif player == 0 and computer == 2:
+    print("Jogador vence")
+
+#situacoes que o player perde
+elif player == 1 and computer == 2:
+    print("Computador vence")
+elif player == 0 and computer == 1:
+    print("Computador vence")
+elif player == 2 and computer == 0:
     print("Computador vence")
 print("o computador jogou {}".format(computer))
