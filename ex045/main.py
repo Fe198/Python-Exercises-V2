0dd9c6bf9009e24befac9488c552291f7fddd2fc
Fe198 <felipeschneider40@gmail.com>
2022-05-01 18:47:24 +0000

    more exercises done

--- ex045/main.py
+++ ex045/main.py
@@ -5,8 +5,39 @@ print("""Suas opções:
 [ 0 ] Pedra
 [ 1 ] Papel
 [ 2 ] Tesoura""")
-player = input("Qual é a sua jogada? ")
+player = int(input("Qual é a sua jogada? "))
 computer = random.randint(0, 2)
+
+time.sleep(0.5)
+print("JO")
+time.sleep(1)
+print("KEN")
+time.sleep(1)
+print("PO")
+time.sleep(0.25)
+
+if player == 0:
+    playerOp = "pedra"
+elif player == 1:
+    playerOp = "papel"
+elif player == 2:
+    playerOp = "tesoura"
+
+if computer == 0:
+    computerOp = "pedra"
+elif computer == 1:
+    computerOp = "papel"
+elif computer == 2:
+    computerOp = "tesoura"
+
+print("=-" * 10)
+print("Computador jogou {}".format(computerOp))
+print("Player jogou {}".format(playerOp))
+print("-=" * 10)
+
+#situação de empate
+if player == computer:
+    print("EMPATE")
 
 # situacoes que o player vence
 if player == 2 and computer == 1:
@@ -17,10 +48,9 @@ elif player == 0 and computer == 2:
     print("Jogador vence")
 
 #situacoes que o player perde
-elif player == 1 and computer == 2:
+if player == 1 and computer == 2:
     print("Computador vence")
 elif player == 0 and computer == 1:
     print("Computador vence")
 elif player == 2 and computer == 0:
     print("Computador vence")
-print("o computador jogou {}".format(computer))
